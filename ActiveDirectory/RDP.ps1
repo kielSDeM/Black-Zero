b034334a63958c6edcc838eeefe15be3326d9642
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-10-14 12:53:37 -0600

    Update RDP.ps1

--- ActiveDirectory/RDP.ps1
+++ ActiveDirectory/RDP.ps1
@@ -1,4 +1,4 @@
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_WORD /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Terminal Server\WinStations\RDP-Tcp" /v SecurityLayer /t REG_DWORD /d 0 /f
 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp" /v UserAuthentication /t REG_DWORD /d 0 /f
 
